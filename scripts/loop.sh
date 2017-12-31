aaf2d401a147fcd7dbfb40fd153a5da3cea90311
kalanda <kalanda@gmail.com>
2017-12-31 01:52:42 +0100

    Enhanced loop.sh

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -1,4 +1,10 @@
+#!/bin/sh
+
 while true; do
-    node setup.js && node test.js
+    node setup.js
+    echo '...'
+    sleep 3
+    echo '...'
+    node test.js
     read -n1 -r -p $'Press any key to continue or Ctrl+C to exit...\n'
 done
