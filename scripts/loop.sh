078a66003db6ef101d6bc9ca851a3f6873888e50
Kalanda <kalanda@gmail.com>
2018-05-21 11:43:56 +0200

    Check for keypress in the loop script at the beginning

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 while true; do
+    read -n1 -r -p $'Press any key to continue or Ctrl+C to exit...\n'
+    clear
     node setup.js
     echo '...'
     sleep 3
     echo '...'
     node test.js
-    read -n1 -r -p $'Press any key to continue or Ctrl+C to exit...\n'
 done
